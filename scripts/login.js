d8f1a9fd6f976756143147cab7c63762edbdcd18
GabrielRodriguesDeveloper <gabrielsousa455@gmail.com>
2023-06-26 20:40:57 -0300

    login validation

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,25 @@
-const getFormValues = () => {
-    const form = document.querySelector("form");
-    
+const validateLogin = (email, password) => {
+    for(let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        if(key != "lastId"){
+            const user = JSON.parse(localStorage.getItem(key));
+            if(email === user.email & password === user.password){
+                console.log("Login Válido");
+                console.log(user.email + " " + user.password);
+                return;
+            }
+        }
+
+    }
 }
+
+
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    validateLogin(email, password);
+    
+})
